01abfe645719a464216713c5ff6ad4f33baeb08b
Tetiana Mykhnevych <neslatpisma@gmail.com>
2017-09-26 06:49:35 +0300

    Initial

---
+++ TrackYourLife-API/TrackYourLife-API/Business/Services/Implementations/ValuesService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrackYourLife.API.Data.Repositories;
+
+namespace TrackYourLife.API.Business.Services.Implementations
+{
+    public class ValuesService : IValuesService
+    {
+        private readonly IValuesRepository _valuesRepository;
+
+        public ValuesService(IValuesRepository valuesRepository)
+        {
+            _valuesRepository = valuesRepository;
+        }
+    }
+}
---
+++ TrackYourLife-API/TrackYourLife-API/Data/Repositories/Implementations/ValuesRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackYourLife.API.Data.Repositories.Implementations
+{
+    public class ValuesRepository : IValuesRepository
+    {
+    }
+}
