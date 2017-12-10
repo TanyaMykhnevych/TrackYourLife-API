530c01478d39a6760b8f08340e8cfa9ae10c4708
Tatyana Mykhnevych <tetiana.mykhnevych@nure.ua>
2017-12-10 17:38:08 +0200

    Publish changes
    Added publish script, changed SQL connection to Azure.

--- Common/Constants/ConfigurationConstants.cs
+++ Common/Constants/ConfigurationConstants.cs
@@ -7,6 +7,9 @@ namespace Common.Constants
     public static class ConfigurationConstants
     {
         //TODO: replace to 'appsettings.json' and retrieve
-        public const string DbConnection = @"Server=.;Database=TrackYourLife;Trusted_Connection=True;MultipleActiveResultSets=true;";
+        // public const string DbConnection = @"Server=.;Database=TrackYourLife;Trusted_Connection=True;MultipleActiveResultSets=true;";
+
+        public const string DbConnection = 
+            @"Server=tcp:mssql-server.database.windows.net,1433;Initial Catalog=TrackYourLife;Persist Security Info=False;User ID=TanyaMy;Password=Test123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
